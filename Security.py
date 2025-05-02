ab480cedb71003a6e2bbf851973973046537d4b9
ee6-lang <ee6-lang@noreply.codeberg.org>
2025-05-02 20:14:57 +0000

    ez python code

---
+++ Security.py
@@ -0,0 +1,13 @@
+print('Welcome to System Security Inc.')
+print('-- where security is our middle name\n')
+
+password = input('Enter your password: ')
+
+if password == "secretspy8374..!":
+    print('Access Granted')
+else:
+        print('Access Denied')
+
+input('\n\nPress the enter key to exit.')
+
+    
