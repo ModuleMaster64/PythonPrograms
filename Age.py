949a1cac6bd53b40ab6be38ecb187fae70850423
ee6-lang <ee6-lang@noreply.codeberg.org>
2025-05-02 20:22:27 +0000

    Add Age.py

---
+++ Age.py
@@ -0,0 +1,9 @@
+age=int(input('What is your age?'))
+if age>=35:
+    print('Congrats!!')
+else:
+  print('You are too young try again when you are old enough')
+      
+  input('\n\nPress the enter key to exit.')
+
+       
