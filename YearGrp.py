137751ad84e66c80189193f8ba596d864a30d7f4
ee6-lang <ee6-lang@noreply.codeberg.org>
2025-05-05 19:16:12 +0000

    python is ez

---
+++ YearGrp.py
@@ -0,0 +1,11 @@
+# Selection commands
+
+# Subroutine to output Key Stage
+def YearGroup(Year):
+    if Year >= 7 and Year <10:
+        print("Key Stage 3!")
+    else:
+        print("Key Stage 4!")
+
+# Main program
+YearGroup(10)
