f497eea69e706962bd4c9071a7c076dd67373ee0
ee6-lang <ee6-lang@noreply.codeberg.org>
2025-05-05 19:14:20 +0000

    hi

---
+++ FootballTeams.py
@@ -0,0 +1,6 @@
+football teams =""
+while name!="Man City":
+    print('Is your favourite football team Man Utd?')
+    print('Is it PSG?')
+    name =input('What\'s your favourite football team?')
+    
